9ab5c160e993dd416054805c79f94859d3ed12e9
John Korzhuk <jkorzhuk@gmail.com>
2017-10-20 12:00:33 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import './config/db';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, err => {
+  if (err) console.error(err);
+  else console.log(`Listening on port ${PORT}`);
+});
